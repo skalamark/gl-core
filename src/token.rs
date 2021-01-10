e9cb77611b746ce8fb17a3c4c66ba84d189c8ca6
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-01-10 19:19:06 -0300

    create structs and impls: (Token, TokenType, TokenPosition)

--- src/token.rs
+++ src/token.rs
@@ -1,1 +1,46 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+use crate::position::Position;
+
+pub struct Token {
+	pub typer: TokenType,
+	pub position: TokenPosition,
+}
+
+pub struct TokenPosition {
+	pub start: Position,
+	pub end: Position,
+}
+
+pub enum TokenType {
+	EOF, // \0
+}
+
+impl Token {
+	pub fn new(typer: TokenType, position: TokenPosition) -> Self {
+		Self { typer, position }
+	}
+}
+
+impl TokenPosition {
+	pub fn new(start: Position, end: Position) -> Self {
+		Self { start, end }
+	}
+
+	pub fn default() -> Self {
+		Self::new(Position::default(), Position::default())
+	}
+
+	pub fn copy(&self) -> Self {
+		Self::new(self.start.copy(), self.end.copy())
+	}
+}
+
+impl TokenType {
+	pub fn is_eof(&self) -> bool {
+		match self {
+			TokenType::EOF => true,
+			_ => false,
+		}
+	}
+}
