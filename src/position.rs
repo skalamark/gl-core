0e3756eced865c737876f489e514707bd90c89b6
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-01-10 19:15:12 -0300

    create struct and impl: Position

--- src/position.rs
+++ src/position.rs
@@ -1,1 +1,20 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+pub struct Position {
+	pub column: usize,
+	pub line: usize,
+}
+
+impl Position {
+	pub fn new(column: usize, line: usize) -> Self {
+		Self { column, line }
+	}
+
+	pub fn default() -> Self {
+		Self::new(0, 0)
+	}
+
+	pub fn copy(&self) -> Self {
+		Self::new(self.column, self.line)
+	}
+}
