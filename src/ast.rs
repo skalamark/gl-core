6e32456849778f2d69e9b87529f1d15a7f3a6155
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-01-11 16:27:04 -0300

    update all structs and enums

--- src/ast.rs
+++ src/ast.rs
@@ -1,20 +1,25 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
 
+#[derive(Debug, PartialEq)]
 pub struct AbstractSyntaxTree {
 	pub statements: Vec<Statement>,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Block(Vec<Statement>);
 
+#[derive(Debug, PartialEq)]
 pub enum Statement {
 	Expression(Expression),
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Expression {
 	Null,
 	Literal(Literal),
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Literal {
 	Integer(i32),
 }
