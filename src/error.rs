392932306799c826a25a6f5ab625e7f405fb3367
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-01-12 21:40:12 -0300

    update

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,50 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
 
+use crate::position::Position;
 use anyhow;
+
+pub type AnyError = anyhow::Error;
+
+#[derive(Debug)]
+pub struct Exception {
+	pub module: String,
+	pub position: Position,
+	pub error: Error,
+}
+
+#[derive(Debug)]
+pub enum ExceptionType {
+	UnexpectedEOF,
+	InvalidSyntax,
+}
 
 #[derive(Debug)]
 pub struct Error {
-	pub class: String,
+	pub class: ExceptionType,
 	pub message: String,
 }
 
-pub type AnyError = anyhow::Error;
+impl std::fmt::Display for Exception {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		writeln!(
+			f,
+			"  File \"{}\", line {} column {}",
+			&self.module,
+			self.position.line + 1,
+			self.position.column
+		)?;
+		write!(f, "{}", &self.error)
+	}
+}
+
+impl std::fmt::Display for ExceptionType {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		match self {
+			ExceptionType::UnexpectedEOF => write!(f, "UnexpectedEOF"),
+			ExceptionType::InvalidSyntax => write!(f, "InvalidSyntax"),
+		}
+	}
+}
 
 impl std::fmt::Display for Error {
 	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
@@ -16,22 +52,29 @@ impl std::fmt::Display for Error {
 	}
 }
 
-impl std::error::Error for Error {}
+impl std::error::Error for Exception {}
 
-impl Error {
-	pub fn custom(class: &str, message: String) -> AnyError {
-		Error {
-			class: format!("{}", class),
-			message,
+impl Exception {
+	pub fn new(module: String, position: Position, error: Error) -> AnyError {
+		Self {
+			module,
+			position,
+			error,
 		}
 		.into()
 	}
+}
 
-	pub fn type_(message: String) -> AnyError {
-		Self::custom("TypeError", message)
+impl Error {
+	pub fn custom(class: ExceptionType, message: String) -> Self {
+		Error { class, message }
 	}
 
-	pub fn invalid_syntax(message: String) -> AnyError {
-		Self::custom("InvalidSyntax", message)
+	pub fn unexpected_eof(message: String) -> Self {
+		Self::custom(ExceptionType::UnexpectedEOF, message)
+	}
+
+	pub fn invalid_syntax(message: String) -> Self {
+		Self::custom(ExceptionType::InvalidSyntax, message)
 	}
 }
