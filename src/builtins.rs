a468a614b1c9b41d19dd0dccb706d377b19fb8ed
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-01-16 15:35:52 -0300

    create fns: (add_builtin, print_fn, println_fn)

--- src/builtins.rs
+++ src/builtins.rs
@@ -17,3 +17,35 @@ impl BuiltinsFns {
 		]
 	}
 }
+
+fn add_builtin(name: &str, param_num: i32, func: BuiltinFn) -> (String, Object) {
+	let name_string: String = String::from(name);
+	(
+		name_string.clone(),
+		Object::Builtin(name_string, param_num, func),
+	)
+}
+
+fn print_fn(args: Vec<Object>) -> Result<Object, AnyError> {
+	let mut fmt_string: String = String::new();
+	for (i, object) in args.iter().enumerate() {
+		fmt_string.push_str(&format!("{}", object));
+		if i < args.len() - 1 {
+			fmt_string.push_str(", ");
+		}
+	}
+	print!("{}", fmt_string);
+	Ok(Object::Null)
+}
+
+fn println_fn(args: Vec<Object>) -> Result<Object, AnyError> {
+	let mut fmt_string: String = String::new();
+	for (i, object) in args.iter().enumerate() {
+		fmt_string.push_str(&format!("{}", object));
+		if i < args.len() - 1 {
+			fmt_string.push_str(", ");
+		}
+	}
+	println!("{}", fmt_string);
+	Ok(Object::Null)
+}
