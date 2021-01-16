d727db007c9b5d80ec46e71e85eebf033b930947
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-01-16 15:40:02 -0300

    update struct and impl: EnvState

--- src/state.rs
+++ src/state.rs
@@ -1,14 +1,18 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
 
 use crate::env::Env;
+use crate::object::Object;
+use std::cell::RefCell;
 use std::collections::HashMap;
+use std::rc::Rc;
 
 pub struct ProgramState {
 	pub env: EnvState,
 }
 
 pub struct EnvState {
-	pub modules: HashMap<String, Env>,
+	pub crate_module: String,
+	pub modules: HashMap<String, Rc<RefCell<Env>>>,
 }
 
 impl ProgramState {
@@ -22,7 +26,30 @@ impl ProgramState {
 impl EnvState {
 	pub fn new() -> Self {
 		Self {
+			crate_module: String::new(),
 			modules: HashMap::new(),
+		}
+	}
+
+	pub fn add_module(&mut self, name: String) {
+		self.modules.insert(name, Rc::new(RefCell::new(Env::new())));
+	}
+
+	pub fn get(&self, identifier: &String, module: &String) -> Option<Object> {
+		if module == "crate" {
+			self.modules[&self.crate_module].borrow().get(identifier)
+		} else {
+			self.modules[module].borrow().get(identifier)
+		}
+	}
+
+	pub fn set(&self, identifier: &String, value: Object, module: &String) {
+		if module == "crate" {
+			self.modules[&self.crate_module]
+				.borrow_mut()
+				.set(identifier, value)
+		} else {
+			self.modules[module].borrow_mut().set(identifier, value)
 		}
 	}
 }
