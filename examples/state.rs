9c00b44faf58e60013f82e2a1d7ce1864790db98
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-01-18 16:50:30 -0300

    add: state.rs

---
+++ examples/state.rs
@@ -0,0 +1,10 @@
+// Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+extern crate gl_core;
+
+use gl_core::state::{EnvState, ProgramState};
+
+fn main() {
+	let _program_state: ProgramState = ProgramState::new();
+	let _env_state: EnvState = EnvState::new();
+}
