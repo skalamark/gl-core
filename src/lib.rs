cf8d14365dc366f6d3e5538ac4fa68c08b60e698
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-02-23 22:47:19 -0300

    create mod: preludes

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,17 @@ pub mod lexer;
 pub mod object;
 pub mod parser;
 pub mod position;
+pub mod source;
 pub mod state;
 pub mod token;
-pub mod utils;
+
+pub mod preludes {
+	pub use crate::ast::*;
+	pub use crate::error::*;
+	pub use crate::lexer::*;
+	pub use crate::object::*;
+	pub use crate::parser::*;
+	pub use crate::position::*;
+	pub use crate::source::*;
+	pub use crate::token::*;
+}
