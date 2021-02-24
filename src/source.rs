063b24c61a603552e92e6a997e20f8534aa9d315
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-02-23 21:43:00 -0300

    create enum: SourceType

--- src/source.rs
+++ src/source.rs
@@ -1,1 +1,9 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+enum SourceType {
+	String(Vec<char>),
+	File(BufReader<File>),
+}
