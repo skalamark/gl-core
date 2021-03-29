ab3971beed8e56ed27fac3e744a1cab1f7721f1a
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-03-29 20:20:22 -0300

    reformat and update methods of impl Env

--- src/env.rs
+++ src/env.rs
@@ -1,47 +1,32 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
 
-use crate::object::Object;
-use std::cell::RefCell;
-use std::collections::HashMap;
-use std::rc::Rc;
+use crate::preludes::*;
 
-#[derive(Clone, Debug, PartialEq)]
 pub struct Env {
 	store: HashMap<String, Object>,
 	parent: Option<Rc<RefCell<Env>>>,
 }
 
 impl Env {
-	pub fn new() -> Self {
-		Self {
-			store: HashMap::new(),
-			parent: None,
-		}
+	pub fn new() -> Self { Self { store: HashMap::new(), parent: None } }
+
+	pub fn from_store(store: HashMap<String, Object>) -> Self { Self { store, parent: None } }
+
+	pub fn from_parent(parent: Rc<RefCell<Env>>) -> Self {
+		Self { store: HashMap::new(), parent: Some(parent) }
 	}
 
-	pub fn from(store: HashMap<String, Object>) -> Self {
-		Self {
-			store,
-			parent: None,
-		}
+	pub fn set<T: Into<String>>(&mut self, identifier: T, value: Object) {
+		self.store.insert(identifier.into(), value);
 	}
 
-	pub fn new_with_parent(parent: Rc<RefCell<Env>>) -> Self {
-		Self {
-			store: HashMap::new(),
-			parent: Some(parent),
-		}
-	}
+	pub fn get<T: Into<String>>(&self, identifier: T) -> Option<Object> {
+		let identifier: String = identifier.into();
 
-	pub fn set(&mut self, identifier: &String, value: Object) {
-		self.store.insert(identifier.clone(), value);
-	}
-
-	pub fn get(&self, identifier: &String) -> Option<Object> {
-		match self.store.get(identifier) {
+		match self.store.get(&identifier) {
 			Some(object) => Some(object.clone()),
-			None => match self.parent {
-				Some(ref parent_env) => parent_env.borrow().get(identifier),
+			None => match &self.parent {
+				Some(parent_env) => parent_env.borrow().get(identifier),
 				None => None,
 			},
 		}
