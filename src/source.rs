fa7ffbce0e0b70639baae84a2382b2c8f7a8007f
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-03-29 20:38:45 -0300

    update enum SourceType and struct Source

--- src/source.rs
+++ src/source.rs
@@ -4,85 +4,75 @@ use std::fs::File;
 use std::io::{self, BufReader, Read};
 
 enum SourceType {
-	String(Vec<char>, String),
+	String(String),
 	File(BufReader<File>),
 }
 
 pub struct Source {
 	chars_cache: Vec<char>,
 	data: SourceType,
+	eof: bool,
 }
 
 impl Source {
-	pub fn from_string(string: String) -> io::Result<Self> {
-		Ok(Self {
-			chars_cache: Vec::new(),
-			data: SourceType::String(Vec::new(), string),
-		})
+	pub fn from_string<T: Into<String>>(string: T) -> Self {
+		Self { chars_cache: Vec::new(), data: SourceType::String(string.into()), eof: false }
 	}
 
-	pub fn from_file(filename: String) -> io::Result<Self> {
-		let file: File = File::open(filename)?;
+	pub fn from_filename<T: Into<String>>(filename: T) -> io::Result<Self> {
+		let file: File = File::open(filename.into())?;
 		Ok(Self {
 			chars_cache: Vec::new(),
 			data: SourceType::File(BufReader::new(file)),
+			eof: false,
 		})
 	}
 
 	pub fn next_char(&mut self) -> Option<char> {
 		if self.chars_cache.len() > 0 {
-			return Some(self.chars_cache.remove(0));
+			let chc: char = self.chars_cache.remove(0);
+			return Some(chc);
 		}
 
 		match &mut self.data {
-			SourceType::String(chars, string) => {
-				if chars.len() > 0 {
-					for _ in 0..10 {
-						if chars.len() > 0 {
-							self.chars_cache.push(chars.remove(0));
-						}
-					}
-				} else if string.len() > 0 {
-					let mut limite_read_chars: usize = 10;
+			SourceType::String(string) =>
+				if !self.eof && string.len() > 0 {
+					let mut limite_read_chars: usize = 32;
 					if string.len() < limite_read_chars {
 						limite_read_chars = string.len();
 					}
 
 					loop {
-						let read_chars: Vec<char> = match string.get(0..limite_read_chars) {
-							Some(read_chars) => read_chars.chars().collect(),
-							None => {
+						let string_buffer: String =
+							if let Some(fstr) = string.get(0..limite_read_chars) {
+								format!("{}", fstr)
+							} else {
 								limite_read_chars += 1;
 								continue;
-							}
-						};
-						let fstring: String =
-							format!("{}", string.get(limite_read_chars..).unwrap());
-						string.clear();
-						string.push_str(&fstring);
+							};
 
-						for one_char in read_chars {
-							chars.push(one_char);
+						{
+							let fstring: String =
+								format!("{}", string.get(limite_read_chars..).unwrap());
+							string.clear();
+							string.push_str(&fstring);
 						}
+
+						self.chars_cache.extend(string_buffer.chars());
 						break;
 					}
-					return self.next_char();
-				}
-			}
-			SourceType::File(file) => {
-				let mut buffer: [u8; 10] = [0; 10];
-				let mut buffer_vec: Vec<u8> = Vec::new();
-				loop {
-					if file.read_exact(&mut buffer).is_err() {
-						buffer_vec.extend(buffer.to_vec());
+				},
+			SourceType::File(file) =>
+				if !self.eof {
+					let mut buffer: [u8; 256] = [0; 256];
+					let n: usize = file.read(&mut buffer).unwrap();
+
+					if let Ok(string_buffer) = String::from_utf8(buffer[..n].to_vec()) {
+						for ch in string_buffer.chars() {
+							self.chars_cache.push(ch);
+						}
 					}
-					let string_buffer: String = String::from_utf8(buffer_vec).unwrap();
-					for one_char in string_buffer.chars() {
-						self.chars_cache.push(one_char);
-					}
-					break;
-				}
-			}
+				},
 		}
 
 		if self.chars_cache.len() > 0 {
