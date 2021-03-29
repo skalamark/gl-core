ce52d1454049602c05f70db894fc20972d870623
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-03-29 20:30:35 -0300

    reformat, add support for espace and rename fn string for: lexe_string

--- src/lexer/lstring.rs
+++ src/lexer/lstring.rs
@@ -4,32 +4,52 @@ use crate::lexer::ResultLexer;
 use crate::preludes::*;
 
 impl Lexer {
-	pub fn string(&mut self) -> ResultLexer {
+	pub fn lexe_string(&mut self) -> ResultLexer {
 		let position_start: Position = self.position.copy();
 		let mut string_literal: String = String::new();
+		let mut escape: bool = false;
 		self.next_char();
 
-		while !self.is_eof_char() && self.cchar != '"' {
-			string_literal.push(self.cchar);
+		while !self.is_eof_char() && (self.cchar != '"' || escape) {
+			if escape {
+				match self.cchar {
+					'\\' => string_literal.push('\\'),
+					'"' => string_literal.push('\"'),
+					'n' => string_literal.push('\n'),
+					'r' => string_literal.push('\r'),
+					't' => string_literal.push('\t'),
+					c => {
+						let mut exception: Exception = Exception::not_runtime(
+							Except::invalid_syntax(format!("unknown character escape: `{}`", c)),
+						);
+						exception.push(ExceptionPoint::new(&self.module, self.position.copy()));
+						return Err(exception);
+					},
+				}
+				escape = false
+			} else {
+				if self.cchar == '\\' {
+					escape = true;
+				} else {
+					string_literal.push(self.cchar);
+				}
+			}
 			self.next_char()
 		}
 
 		if self.cchar != '"' {
-			let mut exception: Exception = Exception::new_not_runtime(Except::unexpected_eof(
-				format!("EOL while scanning string literal"),
-			));
-			exception.push(ExceptionPoint::new(
-				self.module.clone(),
-				self.position.copy(),
-			));
+			let mut exception: Exception =
+				Exception::not_runtime(Except::unexpected_eof("unterminated double quote string"));
+			exception.push(ExceptionPoint::new(&self.module, self.position.copy()));
 			return Err(exception);
 		}
-		self.next_char();
 
-		self.push_token_in_cache(Token::new(
+		self.next_char();
+		self.make_token_and_push(
 			TokenType::STRING(string_literal),
-			TokenPosition::new(position_start, self.position.copy()),
-		));
+			position_start,
+			self.position.copy(),
+		);
 
 		Ok(())
 	}
