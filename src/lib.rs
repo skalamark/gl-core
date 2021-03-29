34a808b2b1cbeaa983832bb34f23fa8d721b2344
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-03-29 20:35:33 -0300

    add pub mod utils and update mod preludes

--- src/lib.rs
+++ src/lib.rs
@@ -10,22 +10,25 @@ pub mod position;
 pub mod source;
 pub mod state;
 pub mod token;
+pub mod utils;
 
 pub mod preludes {
-	pub use anyhow;
-	pub use big_rational_str::*;
-	pub use num::bigint::ToBigInt;
-	pub use num::{BigInt, BigRational, FromPrimitive, ToPrimitive};
 	pub use std::cell::RefCell;
 	pub use std::collections::HashMap;
 	pub use std::rc::Rc;
 
+	pub use num::bigint::ToBigInt;
+	pub use num::{BigInt, BigRational, FromPrimitive, ToPrimitive};
+
 	pub use crate::ast::*;
+	pub use crate::env::*;
 	pub use crate::error::*;
 	pub use crate::lexer::*;
 	pub use crate::object::*;
 	pub use crate::parser::*;
 	pub use crate::position::*;
 	pub use crate::source::*;
+	pub use crate::state::*;
 	pub use crate::token::*;
+	pub use crate::utils::*;
 }
