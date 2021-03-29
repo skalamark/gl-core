3c644b94b57d24e13104f02ad58008cf0e385de5
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-03-29 20:51:16 -0300

    reformat and remove struct and impl EnvState, extend to ProgramState

--- src/state.rs
+++ src/state.rs
@@ -1,52 +1,60 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
 
-use crate::env::Env;
 use crate::preludes::*;
 
 pub struct ProgramState {
-	pub env: EnvState,
-}
-
-pub struct EnvState {
-	pub crate_module: String,
+	crate_module: String,
 	pub modules: HashMap<String, Rc<RefCell<Env>>>,
+	std: Rc<RefCell<Env>>,
 }
 
 impl ProgramState {
-	pub fn new() -> Self {
+	pub fn new<T: Into<String>>(crate_module: T) -> Self {
 		Self {
-			env: EnvState::new(),
-		}
-	}
-}
-
-impl EnvState {
-	pub fn new() -> Self {
-		Self {
-			crate_module: String::new(),
+			crate_module: crate_module.into(),
 			modules: HashMap::new(),
+			std: Rc::new(RefCell::new(Env::new())),
 		}
 	}
 
-	pub fn add_module(&mut self, name: String) {
-		self.modules.insert(name, Rc::new(RefCell::new(Env::new())));
-	}
-
-	pub fn get(&self, identifier: &String, module: &String) -> Option<Object> {
-		if module == "crate" {
-			self.modules[&self.crate_module].borrow().get(identifier)
-		} else {
-			self.modules[module].borrow().get(identifier)
+	pub fn with_std<T: Into<String>>(crate_module: T, std: Env) -> Self {
+		Self {
+			crate_module: crate_module.into(),
+			modules: HashMap::new(),
+			std: Rc::new(RefCell::new(std)),
 		}
 	}
 
-	pub fn set(&self, identifier: &String, value: Object, module: &String) {
-		if module == "crate" {
-			self.modules[&self.crate_module]
-				.borrow_mut()
-				.set(identifier, value)
+	pub fn add_module<T: Into<String>>(&mut self, name: T) {
+		self.modules.insert(name.into(), Rc::new(RefCell::new(Env::from_parent(self.std.clone()))));
+	}
+
+	pub fn add_module_with_env<T: Into<String>>(&mut self, name: T, env: Env) {
+		self.modules.insert(name.into(), Rc::new(RefCell::new(env)));
+	}
+
+	pub fn get<T: Into<String>>(&self, identifier: T, module: T) -> Option<Object> {
+		let identifier: String = identifier.into();
+		let module: String = module.into();
+
+		if let Some(oo) = if module == "crate" {
+			self.modules[&self.crate_module].borrow().get(&identifier)
 		} else {
-			self.modules[module].borrow_mut().set(identifier, value)
+			self.modules[&module].borrow().get(&identifier)
+		} {
+			return Some(oo);
+		} else {
+			self.std.borrow().get(&identifier)
+		}
+	}
+
+	pub fn set<T: Into<String>>(&self, identifier: T, value: Object, module: T) {
+		let module: String = module.into();
+
+		if module == "crate" {
+			self.modules[&self.crate_module].borrow_mut().set(identifier, value)
+		} else {
+			self.modules[&module].borrow_mut().set(identifier, value)
 		}
 	}
 }
