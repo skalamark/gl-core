581796db6224e29d3a49bf8d355c3fdc29a0550e
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-03-29 20:27:59 -0300

    reformat and rename fn whitespace for: lexe_whitespace

--- src/lexer/lwhitespace.rs
+++ src/lexer/lwhitespace.rs
@@ -4,21 +4,23 @@ use crate::lexer::ResultLexer;
 use crate::preludes::*;
 
 impl Lexer {
-	pub fn whitespace(&mut self) -> ResultLexer {
+	pub fn lexe_whitespace(&mut self) -> ResultLexer {
 		let mut position_start: Position = self.position.copy();
 
 		while !self.is_eof_char() && self.cchar.is_whitespace() {
 			if self.cchar == '\n' {
 				self.position.column += 1;
-				self.push_token_in_cache(Token::new(
+				self.make_token_and_push(
 					TokenType::NEWLINE,
-					TokenPosition::new(position_start.copy(), self.position.copy()),
-				));
+					position_start.copy(),
+					self.position.copy(),
+				);
 				self.next_char();
 				self.position.column = 0;
 				self.position.line += 1;
 				continue;
 			}
+
 			self.next_char();
 			position_start = self.position.copy();
 		}
