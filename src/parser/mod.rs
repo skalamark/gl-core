0efe6ae90e71c3767a36afa97a5833cd3bec0f21
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-03-29 21:42:13 -0300

    update struct and impl Parser and add mods: (patom, phashmap, pindex, pvec)

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,43 +2,53 @@
 
 use crate::preludes::*;
 
+mod patom;
 mod pblock;
 mod pcall;
 mod pexpression;
 mod pfunction;
+mod phashmap;
 mod pimport;
+mod pindex;
 mod pinfix;
 mod plet;
 mod pnumber;
 mod pprefix;
 mod pstatement;
+mod pvec;
 
 pub struct Parser {
 	ctoken: Token,
 	ntoken: Token,
+	tokens_cache: Vec<Token>,
 	lexer: Lexer,
 	module: String,
 }
 
 impl Parser {
-	pub fn new(lexer: Lexer) -> Self {
+	pub fn new(lexer: Lexer) -> Result<Self, Exception> {
 		let module: String = lexer.get_module();
 		let mut parser: Self = Self {
 			ctoken: Token::default(),
 			ntoken: Token::default(),
+			tokens_cache: Vec::new(),
 			lexer,
 			module,
 		};
 
-		let _ = parser.next_token(false); // EOF
-		let _ = parser.next_token(true); // EOF
+		let _ = parser.next_token(false)?; // EOF
+		let _ = parser.next_token(true)?; // EOF
 
-		parser
+		Ok(parser)
 	}
 
 	fn next_token(&mut self, while_newline: bool) -> Result<(), Exception> {
 		self.ctoken = self.ntoken.copy();
-		self.ntoken = self.lexer.next()?;
+		if self.tokens_cache.len() > 0 {
+			self.ntoken = self.tokens_cache.remove(0);
+		} else {
+			self.ntoken = self.lexer.next()?;
+		}
 
 		if while_newline {
 			self.next_while_newline()?;
@@ -46,6 +56,8 @@ impl Parser {
 
 		Ok(())
 	}
+
+	fn next_token_from_lexer(&mut self) -> Result<Token, Exception> { self.lexer.next() }
 
 	fn next_while_newline(&mut self) -> Result<(), Exception> {
 		while self.ctoken.typer.is(TokenType::NEWLINE) {
@@ -56,11 +68,12 @@ impl Parser {
 	}
 
 	pub fn next(&mut self) -> Result<Option<Statement>, Exception> {
+		self.next_while_newline()?;
 		if self.ctoken.typer.is(TokenType::EOF) {
 			return Ok(None);
 		}
 
-		Ok(Some(self.statement(true)?))
+		Ok(Some(self.parse_statement(true)?))
 	}
 
 	pub fn run(&mut self) -> Result<AbstractSyntaxTree, Exception> {
