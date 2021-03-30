55e786764f663ff299cc5515959539d5e7f9dfb3
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-03-29 21:17:59 -0300

    create struct: StructRust

--- src/object/structrust.rs
+++ src/object/structrust.rs
@@ -16,3 +16,26 @@ pub trait ObjectTrait: std::fmt::Display + std::fmt::Debug + DynClone + Downcast
 impl PartialEq for Box<dyn ObjectTrait> {
 	fn eq(&self, other: &Self) -> bool { self.is_equals(other) }
 }
+
+#[derive(Debug)]
+pub struct StructRust {
+	pub o: Box<dyn ObjectTrait>,
+	pub env: HashMap<String, FNStructRust>,
+}
+
+impl StructRust {
+	pub fn new(o: Box<dyn ObjectTrait>) -> Self {
+		let env: HashMap<String, FNStructRust> = o.get_env();
+		Self { o, env }
+	}
+}
+
+impl Clone for StructRust {
+	fn clone(&self) -> Self { Self::new(self.o.clone()) }
+}
+
+impl PartialEq for StructRust {
+	fn eq(&self, other: &Self) -> bool { self.o == other.o.clone() }
+}
+
+impl Eq for StructRust {}
