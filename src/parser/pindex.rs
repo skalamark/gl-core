4599cb19b5143a87479e2b6f9319443eb709f435
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-03-29 21:39:16 -0300

    create fn for impl Parser: parse_index

--- src/parser/pindex.rs
+++ src/parser/pindex.rs
@@ -1,1 +1,20 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+use crate::preludes::*;
+
+impl Parser {
+	pub fn parse_index(&mut self, left: Expression) -> Result<Expression, Exception> {
+		self.next_token(true)?; // LeftBracket
+		let index: Expression = self.parse_expression(Precedence::Lowest)?;
+
+		if !self.ctoken.typer.is(TokenType::RightBracket) {
+			let mut exception: Exception =
+				Exception::not_runtime(Except::invalid_syntax("expected ']'"));
+			exception.push(ExceptionPoint::new(&self.module, self.ctoken.position.start.copy()));
+			return Err(exception);
+		}
+
+		self.next_token(false)?; // RightBracket
+		Ok(Expression::Index(Box::new(left), Box::new(index)))
+	}
+}
