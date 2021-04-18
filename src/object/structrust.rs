7a5c171404bf3e7a30caab6f940038d8bcfb15c8
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-04-18 15:45:44 -0300

    remove dyn_clone and update: (ObjectTrait, StructRust)
    create fns for ObjectTrait: (display, clone) and remove fn: get_env
    create fn for StructRust: add_function
    update: impl (Clone, PartialEq) for StructRust
    impl Display for StructRust

--- src/object/structrust.rs
+++ src/object/structrust.rs
@@ -1,23 +1,21 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
 
-use downcast_rs::{impl_downcast, DowncastSync};
-use dyn_clone::{clone_trait_object, DynClone};
+use downcast_rs::{impl_downcast, Downcast};
 
 use crate::preludes::*;
 
-impl_downcast!(sync ObjectTrait);
-clone_trait_object!(ObjectTrait);
+impl_downcast!(ObjectTrait);
 
-pub trait ObjectTrait: std::fmt::Display + std::fmt::Debug + DynClone + DowncastSync {
+pub trait ObjectTrait: Downcast {
 	fn is_equals(&self, other: &Box<dyn ObjectTrait>) -> bool;
-	fn get_env(&self) -> HashMap<String, FNStructRust> { HashMap::new() }
+	fn display(&self) -> String { format!("tst") }
+	fn clone(&self) -> Box<dyn ObjectTrait>;
 }
 
 impl PartialEq for Box<dyn ObjectTrait> {
 	fn eq(&self, other: &Self) -> bool { self.is_equals(other) }
 }
 
-#[derive(Debug)]
 pub struct StructRust {
 	pub o: Box<dyn ObjectTrait>,
 	pub env: HashMap<String, FNStructRust>,
@@ -25,17 +23,31 @@ pub struct StructRust {
 
 impl StructRust {
 	pub fn new(o: Box<dyn ObjectTrait>) -> Self {
-		let env: HashMap<String, FNStructRust> = o.get_env();
+		let env: HashMap<String, FNStructRust> = HashMap::new();
 		Self { o, env }
+	}
+
+	pub fn add_function(&mut self, name: String, value: FNStructRust) {
+		self.env.insert(name, value);
 	}
 }
 
 impl Clone for StructRust {
-	fn clone(&self) -> Self { Self::new(self.o.clone()) }
+	fn clone(&self) -> Self {
+		let mut s = Self::new(self.o.clone());
+		s.env = self.env.clone();
+		s
+	}
+}
+
+impl std::fmt::Display for StructRust {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+		write!(f, "{}", self.o.display())
+	}
 }
 
 impl PartialEq for StructRust {
-	fn eq(&self, other: &Self) -> bool { self.o == other.o.clone() }
+	fn eq(&self, other: &Self) -> bool { &self.o == &other.o }
 }
 
 impl Eq for StructRust {}
