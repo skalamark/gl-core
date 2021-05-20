d502275cefaf6c801ff6f3a748a9ceb2a6271f49
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-05-20 18:16:31 -0300

    fix: tuple expression parse

--- src/parser/ptuple.rs
+++ src/parser/ptuple.rs
@@ -15,26 +15,30 @@ impl Parser {
 			values.push(first);
 		}
 
-		while !self.ctoken.typer.is(TokenType::RightParen) {
+		while (exists_first_value)
+			|| (!exists_first_value && !self.ctoken.typer.is(TokenType::RightParen))
+		{
 			values.push(self.parse_expression(Precedence::Comma)?);
 			self.next_while_newline()?;
 
 			match &self.ctoken.typer {
 				TokenType::COMMA => self.next_token(true)?, // COMMA
-				TokenType::RightParen => {},
-				_ if exists_first_value => break,
-				_ => {
-					let mut exception: Exception = Exception::not_runtime(Except::invalid_syntax(
-						format!("expected ',' or ')'"),
-					));
+				TokenType::RightParen if !exists_first_value => {},
+				_ if !exists_first_value => {
+					let mut exception: Exception =
+						Exception::not_runtime(Except::invalid_syntax("expected ',' or ')'"));
 					exception
 						.push(ExceptionPoint::new(&self.module, self.ctoken.position.start.copy()));
 					return Err(exception);
 				},
+				_ => break,
 			}
 		}
 
-		self.next_token(false)?; // RightParen
+		if !exists_first_value {
+			self.next_token(false)?; // RightParen
+		}
+
 		if values.len() == 1 {
 			Ok(values.remove(0))
 		} else {
