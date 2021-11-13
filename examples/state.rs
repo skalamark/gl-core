41e134f1d647c211b62a41339949b74462223a9f
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-11-13 18:00:24 -0300

    delete state.rs

--- examples/state.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
-
-extern crate gl_core;
-
-use gl_core::preludes::*;
-
-fn main() {
-	let _program_state: ProgramState = ProgramState::new("examples/state/program_state_new");
-}
